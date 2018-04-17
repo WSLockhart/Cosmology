049c7bd306790c7831f635f484908463d85e6ea5
edschenk785 <38273492+edschenk785@users.noreply.github.com>
2018-04-16 21:21:32 -0700

    Add files via upload
    Run scripts in order given in mAsTeR.m

---
+++ mAsTeR.m
@@ -0,0 +1,27 @@
+% The Master Script??? 4/16/2017 V0.01
+
+% Run step1.m
+% Run random_masses.m
+% Run density_rho.m
+% Run potential_phi.m
+% Run Will.m
+
+% 1)
+
+%step1
+
+% 2)
+
+%random_masses
+random_masses2D
+
+% Loop these sections
+
+% 3)
+
+density_rho
+
+% 4)
+
+%potential_phi
+potential_phi2D
---
+++ potential_phi2D.m
@@ -0,0 +1,34 @@
+% Script finding the potential phi By EED
+
+% Calculating average density and overdensity
+rhoav = mean(rho(:));
+deltax = rho - 1;
+
+% Start solving the discretized Poisson Equation
+% Need to solve in k-space where the equation becomes phik = Gr*deltak
+% Get the overdensity in k-space using FFT
+
+deltak = fftn(deltax);
+
+% Finding phi in fourier space
+for l = 1:Ng
+    for m = 1:Ng
+        for n = 1:Ng
+            phik(m,l,n)= Gr(l,m,n)*deltak(l,m,n);
+        end
+    end
+end
+
+% Now we FFT phik back to configuration space to get phi
+phi = circshift(circshift(circshift(...  Shifting is to align the potential field to the mass positions
+    real( ...   potential must be real
+    ifftn(phik) ...
+    ),-1,1),-1,2),-1,3);
+
+%plotting bullshit
+hold off
+contourf(phi(:,:,1))
+hold on
+%scatter3(x,y,z)
+axis([0 Ng 0 Ng 0 Ng])
+grid on
