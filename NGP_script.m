9699e8f9bfbba53f071aeec3a443a25decf40899
Will Lockhart <31291365+WSLockhart@users.noreply.github.com>
2018-04-25 18:20:57 -0700

    Add files via upload

--- NGP_script.m
+++ NGP_script.m
@@ -1,8 +1,8 @@
 % Particle Mesh code for Astro class by MEEEE Ed Schenk  Mar. 30 2018
 
 % Set number of particles and grid cells
-Np = 1000;
-Ng = 10;
+Np = 8;
+Ng = 128;
 
 % Set some variables
 mp = 1;
@@ -11,10 +11,11 @@ OmegaM = 1;
 a = 1;
 H0 = 1;
 G = 1;
-rho0 = (3*H0^2/(8*pi*G))*OmegaM;
+rho0 = 1;
+%rho0 = (3*H0^2/(8*pi*G))*OmegaM;
 
 % Declare position and velocity arrays for each particle
-z = zeros(Np,1);
+x = zeros(Np,1);
 y = zeros(Np,1);
 z = zeros(Np,1);
 
@@ -25,6 +26,7 @@ vz = zeros(Np,1);
 % Declaring density and potential arrays for each grid point
 rho = zeros(Ng,Ng,Ng);
 phi = zeros(Ng,Ng,Ng);
+phik= zeros(Ng,Ng,Ng);
 % Declaring Green's function
 Gr = zeros(Ng,Ng,Ng);
 
@@ -36,7 +38,7 @@ y = Ng*rand(Np,1);
 z = Ng*rand(Np,1);
 
 % Plotting initial points
-scatter3(x,y,z)
+%scatter3(x,y,z)
 axis([0 Ng 0 Ng 0 Ng])
 hold on
 
@@ -47,7 +49,7 @@ k = mod(round(z), Ng);
 
 % Placing each point into the density array
 for n = 1:Np
-    rho(i(n)+1,j(n)+1,k(n)+1)= rho(i(n)+1,j(n)+1,k(n)+1) + mp/rho0;
+    rho(i(n)+1,j(n)+1,k(n)+1)= rho(i(n)+1,j(n)+1,k(n)+1) + 1;
 end
 
 % Average density
@@ -55,7 +57,7 @@ rhoav = mean(rho(:));
 
 deltax = rho - 1;
 
-% Plotting nearest gid points
+% Plotting nearest grid points
 scatter3(i,j,k);
 hold off
 
@@ -71,8 +73,29 @@ for l = 1:Ng
             if (l==1)&&(m==1)&&(n==1)
                 Gr(1,1,1)=0;
             else
-                Gr(l,m,n)= -(((3*OmegaM)/(8*a))/(sin(pi*(l-1)/Ng)^2+sin(pi*(m-1)/Ng)^2+sin(pi*(n-1)/Ng)^2));
+                Gr(l,m,n)= -(1/(sin(pi*(l-1)/Ng)^2+sin(pi*(m-1)/Ng)^2+sin(pi*(n-1)/Ng)^2));
             end
         end
     end
 end
+
+% Finding phi in fourier space
+for l = 1:Ng
+    for m = 1:Ng
+        for n = 1:Ng
+            phik(m,l,n)= Gr(l,m,n)*deltak(l,m,n);
+        end
+    end
+end
+
+% Now we FFT phik back to configuration space to get phi
+phi = circshift(circshift(circshift(...  Shifting is to align the potential field to the mass positions
+    real( ...   potential must be real
+    ifftn(phik) ...
+    ),-1,1),-1,2),-1,3);
+
+%plotting bullshit
+contourslice(phi,i,j,k)
+view(3)
+grid on
+
---
+++ random_masses2D.m
@@ -0,0 +1,4 @@
+% random_masses
+x = Ng*rand(Np,1);
+y = y+Ng/2;
+z = z+Ng/2;
